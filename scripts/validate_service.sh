952facdcf0065af1c1723209100d0528e7c6f7dc
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2021-01-12 20:13:47 +0000

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # verify we can access our webpage successfully
+read -t 15 -p "I am going to wait for 15 seconds only ..."
 curl -v --silent localhost:8080/actuator/health 2>&1 | grep UP
