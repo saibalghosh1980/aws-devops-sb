68924a21835ffcb7283767321f90a30b156e9b36
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2021-01-12 12:27:31 +0000

    NA

---
+++ awssbdemo/src/main/java/com/example/awssbdemo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.awssbdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping(value = "/hello/{name}")
+	public String sayHello(@PathVariable("name") String name) {
+		
+		return "Hello "+name.toUpperCase();
+	}
+
+}
---
+++ awssbdemo/src/test/java/com/example/awssbdemo/AwssbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awssbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwssbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
