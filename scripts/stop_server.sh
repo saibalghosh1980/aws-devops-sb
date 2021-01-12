4133106e6d77912235c14af8fee516db4381ed19
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2021-01-12 20:07:51 +0000

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Kill Java process
 # Determine the pid
-isExistApp = `pgrep java`
+isExistApp=`pgrep java`
 if [[ -n  $isExistApp ]]; then
     PID=`ps -C java -o pid=`
     kill -9 $PID
